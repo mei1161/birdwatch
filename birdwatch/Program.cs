4638747a4eac595a77741f0fa8630cb09c624111
たけだ <ichigo_happy@ezweb.ne.jp>
2019-09-27 11:17:29 +0900

    console proj

---
+++ birdwatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace birdwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
