1f0b7ba3ce44f79f626f86898c0eea1baab55161
たけだ <ichigo_happy@ezweb.ne.jp>
2019-10-04 10:19:39 +0900

    csファイルを追加する。

--- birdwatch.Tests/UnitTest1.cs
+++ birdwatch.Tests/UnitTest1.cs
@@ -1,15 +1,30 @@
 using birdwatch;
 using NUnit.Framework;
 using System.Linq;
+using System.Collections.Generic;
 namespace birdwatch.Tests
 {
+    public class twitterApi
+    {
+        public IEnumerable<string> GetFollowers()
+        {
+            yield return "@mei";
+        }
+    }
+
     public class Tests
     {
+        //Given: 自分
+        //When: フォロワー一覧を取得
+        //Then: meiを取得できる
+        //GWTStyle
+
         [Test]
-        public void Test1()
+        public void 自分のアカウントからフォロワー取得できる()
         {
-            string i = Program.GetWhen();
-            Assert.That(i.Equals("Twitter"));
+            var twiterapi = new twitterApi();
+            var followers =twiterapi.GetFollowers();
+            Assert.That(followers, Is.SupersetOf(new[] { "@mei" }));
         }
     }
 }
---
+++ birdwatch/twitterapi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace birdwatch
+{
+    public class TwitterApi
+    {
+    }
+}
