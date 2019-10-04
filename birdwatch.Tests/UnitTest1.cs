6f703be215ddd92af73c64f3ec5a4e39cf7c197c
芽衣 武田 <ichigo_happy@ezweb.ne.jp>
2019-10-04 22:44:24 +0900

    JSONファイルから、設定ファイルを読み込む。

--- birdwatch.Tests/UnitTest1.cs
+++ birdwatch.Tests/UnitTest1.cs
@@ -2,6 +2,8 @@ using birdwatch;
 using NUnit.Framework;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 namespace birdwatch.Tests
 {
     public class MockTwitterApi :ITwitterApi
@@ -26,5 +28,15 @@ namespace birdwatch.Tests
             var followers =twiterapi.GetFollowers();
             Assert.That(followers, Is.SupersetOf(new[] { "@mei" }));
         }
+
+        [Test]
+        public void 実際のAPIでmeiのアカウントを指定してフォロワーを取得するとayaが取得できる()
+        {
+            var configuration = Configuration.Parse(@"conf\birdwatch.json"); 
+            var twitterApi = new TwitterApi(configuration);
+            var followers = twitterApi.GetFollowers();
+            Assert.That(followers, Is.SupersetOf(new[] { "@nectarim" }));
+        }
+
     }
 }
--- birdwatch/twitterapi.cs
+++ birdwatch/twitterapi.cs
@@ -4,31 +4,67 @@ using System.Linq;
 using System.Threading;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.IO;
 
 namespace birdwatch
 {
+    public class Configuration
+    {
+        public string ConsumerKey { get; set; }
+        public string ConsumerSercret { get; set; }
+        public string AccsessToken { get; set; }
+        public string AccsessSercret { get; set; }
+
+        public Configuration(string consumerKey, string consumerSercret, string accsessToken, string accsessSercret)
+        {
+            this.ConsumerKey = consumerKey;
+            this.ConsumerSercret = consumerSercret;
+            this.AccsessToken = accsessToken;
+            this.AccsessSercret = accsessSercret;
+        }
+        public Tokens CreateTokens()
+        {
+            return Tokens.Create(ConsumerKey, ConsumerSercret, AccsessToken, AccsessSercret);
+        }
+
+        public Configuration()
+        {
+        }
+
+        public static Configuration Parse(string path)
+        {
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<Configuration>(json);
+        }
+    }
+
     public interface ITwitterApi
     {
         public IEnumerable<string> GetFollowers();
     }
+    // GoF Adapter pattern
     public class TwitterApi : ITwitterApi
     {
+        private readonly Configuration configuration; 
+        public TwitterApi(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
         public IEnumerable<string> GetFollowers()
         {
-            var tokens = Tokens.Create("", "", "", "");
-
+            var tokens = configuration.CreateTokens(); 
             var users = tokens.Followers;
-
             var followers = new List<User>();
-
             var nextCursor = -1L;
-
-            while(true)
+            while (true)
             {
-
+                var list = users.List("mei_9961", cursor: nextCursor, count: 200);
+                followers.AddRange(list);
+                if (list.Count == 0) break;
+                nextCursor = list.NextCursor;
             }
-
-            
+            return followers.Select(u => "@" + u.ScreenName);
         }
     }
 }
