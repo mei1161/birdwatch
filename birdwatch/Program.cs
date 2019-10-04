90d27f5dcd371a66a801aa4882f93fd8c461f15e
たけだ <ichigo_happy@ezweb.ne.jp>
2019-10-04 09:59:50 +0900

    不要なプロジェクトを削除

--- birdwatch/Program.cs
+++ birdwatch/Program.cs
@@ -2,22 +2,13 @@
 using System.Collections.Generic;
 namespace birdwatch
 {
-public class Account
+    public class Account
     {
-        public string AccountName;//アカウント名
-        
-        public string GetAccount(string val)//アカウント名取得
+        static void Main()
         {
-            if (val.Equals(AccountName))
-            {
-                return AccountName;
-            }
-            else
-            {
-                return "NoAccount";
-            }
-        }
 
-    }
+        }
+    } 
+
 
 }
